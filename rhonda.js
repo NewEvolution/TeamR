c1a41d298260e5648c334971616c2fa91769f638
Rhonda Eisenhauer <rhonda.s.eisenhauer@live.com>
2015-07-29 14:42:50 -0500

    basic rhonda.js added

---
+++ rhonda.js
@@ -0,0 +1,28 @@
+var Gibberish = (function(lingo) {
+
+  var  Italian = {
+
+    merry: "allegro",
+    christmas: "natale",
+    and: "e",
+    happy: "felice",
+    new: "nuovo",
+    year: "anno"
+} 
+
+  
+lingo.translateToItalian = function(input) {
+
+}   
+
+
+
+
+  return Gibberish
+  
+
+  
+//IIFE = This whole thing is an IIFE! Immediatelt Invoked Function Expression ()//
+})(Gibberish);
+
+console.log(Sun.getdwarfPlanets());
